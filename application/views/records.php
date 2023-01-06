b2335ec15242941b2afca5b8e42c0f829e8e3aa4
E-ringz <ron.garcia.santos@gmail.com>
2023-01-06 13:27:03 +0800

    update

--- application/views/records.php
+++ application/views/records.php
@@ -7,6 +7,7 @@
 	<title>Attendance Party</title>
 	<link rel="stylesheet" href="/assets/css/style.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js">
 	<style>
 		.attendance-head img{
                 display: inline-block;
