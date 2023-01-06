aacc8cd9fd75664091548fa4c0ba284a82d2ef62
E-ringz <ron.garcia.santos@gmail.com>
2023-01-06 15:20:47 +0800

    update

--- application/models/User.php
+++ application/models/User.php
@@ -3,16 +3,17 @@
 
     class User extends CI_Model
     {
-        function get_attendance_of_the_day($subject_code)
+        function display_attendance_of_the_day($subject_code)
         {
             $query = "SELECT users.id, first_name, last_name, 
             roles.id AS role_id, role, 
             subject_code, subject_name, subject_sched,
-            seat.id AS seat_no  
+            seats.id AS seat_no  
             FROM attendances
             INNER JOIN users ON users.id = attendances.user_id
             INNER JOIN subjects ON subjects.id = attendances.subject_id
             INNER JOIN seats ON seats.id = attendances.seat_id
+            LEFT JOIN roles ON roles.id = users.role_id
             WHERE subject_code = ?";
             $values = array($subject_code);
             return $this->db->query($query, $values)->result_array();
