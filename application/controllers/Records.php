6eae2c28f7fc824113b0f2cc0c2ddb799fa119d3
E-ringz <ron.garcia.santos@gmail.com>
2023-01-07 15:05:39 +0800

    update

--- application/controllers/Records.php
+++ application/controllers/Records.php
@@ -21,7 +21,7 @@ class Records extends CI_Controller {
 	public function index()
 	{
 		$subject_code = "APPSDEV1";
-		$result['records'] = $this->user->display_attendance_of_the_day($subject_code);
+		$result['records'] = $this->record->get_attendance_of_the_day($subject_code);
 
 		$subject_name = $this->subject->get_subject($subject_code);
 		$result['subject'] = implode("", $subject_name);
---
+++ application/models/Record.php
@@ -0,0 +1,21 @@
+<?php
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Record extends CI_Model
+    {
+        function get_attendance_of_the_day($subject_code)
+        {
+            $query = "SELECT users.id, first_name, last_name, 
+            roles.id AS role_id, role, 
+            subject_code, subject_name, subject_sched,
+            seats.id AS seat_no  
+            FROM attendances
+            INNER JOIN users ON users.id = attendances.user_id
+            INNER JOIN subjects ON subjects.id = attendances.subject_id
+            INNER JOIN seats ON seats.id = attendances.seat_id
+            LEFT JOIN roles ON roles.id = users.role_id
+            WHERE subject_code = ?";
+            $values = array($subject_code);
+            return $this->db->query($query, $values)->result_array();
+        }
+    }
